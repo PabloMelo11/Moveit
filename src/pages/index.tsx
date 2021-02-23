d39f3c2350dcc9274cd22abd5808aa70972def7c
PabloMelo <pablomelodev@gmail.com>
2021-02-23 10:55:45 -0300

    feat: add favicon and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import Head from 'next/head';
+
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExpirienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Perfil';
 
@@ -7,12 +10,17 @@ import styles from '../styles/pages/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExpirienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
