77538ee901d9576b8a9ea3e09efe5c3d61a03d42
PabloMelo <pablomelodev@gmail.com>
2021-02-24 15:33:06 -0300

    feat: finished challenge box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExpirienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Perfil';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -22,7 +23,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
